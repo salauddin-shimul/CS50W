75e4bfec647be9948cf31f98c6f832c9620353b1
SM Salauddin Shimul <sm.salauddin.shimul@g.bracu.ac.bd>
2024-07-27 13:50:48 +0600

    modified func_list and square files to check if cube_num function works

--- python_files/func_list.py
+++ python_files/func_list.py
@@ -1,2 +1,5 @@
 def square(n):
     return n**2
+
+def cube_num(n):
+    return n**3
--- python_files/square.py
+++ python_files/square.py
@@ -1,5 +1,10 @@
 from func_list import square
+from func_list import cube_num
 
-for i in range(1,11):
+'''
+for i in range(1,10):
     print(f"The square of {i} is {square(i)}.")
-    
+'''    
+
+for i in range(1, 10):
+    print(f"Cube of {i} is {cube_num(i)}.")
