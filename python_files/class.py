505438f0f8af53982021609b900257a6346ab4eb
SM Salauddin Shimul <sm.salauddin.shimul@g.bracu.ac.bd>
2024-08-04 20:17:06 +0600

    added add_passenger and open_seats methods to add new passengers and check available seats

--- python_files/class.py
+++ python_files/class.py
@@ -7,3 +7,18 @@ p = Point(3, 9)
 print(p.x)
 print(p.y)'''
 
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passenger = []
+    
+    def add_passenger(self, name):
+        if self.open_seats() == 0:
+            return False
+        self.passenger.append(name)
+        return True
+               
+    def open_seats(self):
+        return self.capacity - len(self.passenger)
+        
+flight_1 = Flight(3)
